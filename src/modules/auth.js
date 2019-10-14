72c6d88bfd2a66e81384cfb61fcfd99c854e10af
Jang Haemin <io@jhaemin.com>
2019-10-14 23:11:50 +0900

    refactor: require once

--- src/modules/auth.js
+++ src/modules/auth.js
@@ -1,14 +1,15 @@
+const conn = require('@/db/db-connector').getConnection()
+const crypto = require('crypto')
+
 class Auth {
   static async signin(session, options) {
     const { portalId, password } = options
 
-    const crypto = require('crypto')
     const pwHash = crypto
       .createHash('sha256')
       .update(password)
       .digest('base64')
 
-    const conn = require('@/db/db-connector').getConnection()
     const query = 'SELECT * FROM users WHERE portal_id = ? AND password = ?'
     const values = [portalId, pwHash]
     const queryResult = await new Promise(resolve => {
@@ -37,6 +38,8 @@ class Auth {
       }
     }
   }
+
+  static async register(options) {}
 }
 
 module.exports = Auth
