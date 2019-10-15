68535df357ddb7e37a8ff1e89e07b7643c18f903
Jang Haemin <io@jhaemin.com>
2019-10-15 15:58:54 +0900

    feat: sign up

--- src/api/auth/index.js
+++ src/api/auth/index.js
@@ -3,9 +3,16 @@ const authRouter = new Router()
 const Auth = require('@/modules/auth')
 
 authRouter.get('/sign-in', async (ctx, next) => {
-  ctx.body = await Auth.signin(ctx.session, ctx.request.body)
+  ctx.body = await Auth.signIn(ctx.session, ctx.request.body)
 })
 
-authRouter.post('/register', async (ctx, next) => {})
+authRouter.post('/sign-up', async (ctx, next) => {
+  const requestData = ctx.request.body
+  ctx.body = await Auth.signUp(requestData)
+})
+
+authRouter.get('/sign-out', (ctx, next) => {
+  ctx.body = Auth.signOut(ctx.session)
+})
 
 module.exports = authRouter
