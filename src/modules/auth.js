618bf42d024b6699e8d0408f5b48e90be1052ecf
Jang Haemin <io@jhaemin.com>
2019-10-15 15:58:54 +0900

    feat: sign up

--- src/modules/auth.js
+++ src/modules/auth.js
@@ -1,45 +1,114 @@
 const conn = require('@/db/db-connector').getConnection()
 const crypto = require('crypto')
+const dayjs = require('dayjs')
 
 class Auth {
-  static async signin(session, options) {
-    const { portalId, password } = options
-
-    const pwHash = crypto
+  static hashPw(password) {
+    return crypto
       .createHash('sha256')
       .update(password)
       .digest('base64')
+  }
+
+  static async signIn(session, options) {
+    const { portalId, password } = options
+
+    if (!portalId) {
+      return {
+        err: 1
+      }
+    }
+
+    if (!password) {
+      return {
+        err: 2
+      }
+    }
+
+    const pwHash = this.hashPw(password)
 
     const query = 'SELECT * FROM users WHERE portal_id = ? AND password = ?'
     const values = [portalId, pwHash]
-    const queryResult = await new Promise(resolve => {
+    const { err, results } = await new Promise(resolve => {
       conn.query(query, values, (err, results) => {
-        if (err) {
-          resolve({
-            err: err.stack
-          })
-          return
-        }
-
-        resolve({
-          err: false,
-          rows: results
-        })
+        resolve({ err, results })
       })
     })
 
-    console.log(queryResult.rows)
+    if (!err && results.length === 1) {
+      session.auth = results[0]
 
-    if (!queryResult.err && queryResult.rows.length === 1) {
-      return true
+      return {
+        err: false
+      }
     } else {
       return {
-        err: true
+        err: 6
       }
     }
   }
 
-  static async register(options) {}
+  static async signUp(options) {
+    const { portalId, password, nickname } = options
+
+    if (!portalId) {
+      return {
+        err: 3
+      }
+    }
+
+    if (!password) {
+      return {
+        err: 4
+      }
+    }
+
+    const query = 'SELECT * FROM users WHERE portal_id = ?'
+    const values = [portalId]
+    const { results } = await new Promise(resolve => {
+      conn.query(query, values, (err, results) => {
+        resolve({ err, results })
+      })
+    })
+
+    if (results.length === 0) {
+      // Available
+      const query2 =
+        'INSERT INTO users (portal_id, password, registered_at, nickname) VALUES (?, ?, ?, ?)'
+      const values2 = [
+        portalId,
+        this.hashPw(password),
+        dayjs().format('YYYY-MM-DD HH:mm:ss'),
+        nickname
+      ]
+      const { err } = await new Promise(resolve => {
+        conn.query(query2, values2, (err, results) => {
+          resolve({ err, results })
+        })
+      })
+
+      if (!err) {
+        return {
+          err: false
+        }
+      }
+    } else if (results.length === 1) {
+      // There already exists a user with this portal ID
+      return {
+        err: 5
+      }
+    } else {
+      // Fatal error
+      // Must be reviewed by the maintainers
+      return {
+        err: 7
+      }
+    }
+  }
+
+  static signOut(session) {
+    session = null
+  }
 }
 
 module.exports = Auth
