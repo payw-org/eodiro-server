bd996aa2a8b1a2be083bb232fa2f0d8d2d46d97a
Jang Haemin <io@jhaemin.com>
2019-10-22 16:04:05 +0900

    refactor: rename auth module result

--- src/api/auth/index.js
+++ src/api/auth/index.js
@@ -3,19 +3,19 @@ const router = express.Router()
 const Auth = require('@/modules/auth')
 
 router.get('/sign-in', async (req, res) => {
-  const body = await Auth.signIn(null, req.body)
-  res.send(body)
+  const result = await Auth.signIn(null, req.body)
+  res.send(result)
 })
 
 router.post('/sign-up', async (req, res) => {
   const requestData = req.body
-  const body = await Auth.signUp(requestData)
-  res.send(body)
+  const result = await Auth.signUp(requestData)
+  res.send(result)
 })
 
 router.get('/sign-out', (req, res) => {
-  const body = Auth.signOut(null)
-  res.send(body)
+  const result = Auth.signOut(null)
+  res.send(result)
 })
 
 module.exports = router
