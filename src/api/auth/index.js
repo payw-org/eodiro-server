adc06cbfea782ce88f2c53ade807afaf70391e06
Jang Haemin <io@jhaemin.com>
2019-10-23 22:45:24 +0900

    feat: create and test automated bot

--- src/api/auth/index.js
+++ src/api/auth/index.js
@@ -2,6 +2,10 @@ const express = require('express')
 const router = express.Router()
 const Auth = require('@/modules/auth')
 
+const Bot = require('@/modules/bot')
+const bot = new Bot()
+bot.clearPendingUsers()
+
 router.get('/sign-in', async (req, res) => {
   const result = await Auth.signIn(req.session, req.body)
   res.send(result)
