55a12e7e8955e46524127f1f227adfb80056541b
Jang Haemin <io@jhaemin.com>
2019-10-23 12:33:40 +0900

    Build mailer module

--- src/modules/mailer.js
+++ src/modules/mailer.js
@@ -1,3 +1,29 @@
-class Maler {}
+const nodeMailer = require('nodemailer')
 
-export default Mailer
+class Mailer {
+  /**
+   * @param {Object} options
+   * @param {string} options.to
+   * @param {string} options.html
+   */
+  static sendMail(options) {
+    this.transporter.sendMail({
+      from: '"어디로" <contact@payw.org>',
+      to: options.to,
+      html: options.html
+    })
+  }
+}
+
+Mailer.transporter = nodeMailer.createTransport({
+  service: process.env.MAIL_SERVICE,
+  host: process.env.MAIL_HOST,
+  port: parseInt(process.env.MAIL_PORT),
+  secure: true,
+  auth: {
+    user: process.env.MAIL_USERNAME,
+    pass: process.env.MAIL_PASSWORD
+  }
+})
+
+module.exports = Mailer
