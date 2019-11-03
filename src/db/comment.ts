8abaf90a80ad7d867c0949c4e134f3d9f6e8d9f0
Jang Haemin <io@jhaemin.com>
2019-11-03 11:11:36 +0900

    feat: add a comment

--- src/db/comment.ts
+++ src/db/comment.ts
@@ -1,3 +1,5 @@
+import Db from '@/db'
+
 export interface CommentModel {
   id: number
   post_id: number
@@ -5,4 +7,37 @@ export interface CommentModel {
   uploaded_at: string
   user_id: number
   likes: number
+}
+
+export interface CommentNew {
+  postId: number
+  userId: number
+  body: string
+}
+
+export default class Comment {
+  static isValidBody(body: string): boolean {
+    return body.length > 0
+  }
+
+  static async add(commentData: CommentNew): Promise<boolean> {
+    if (!this.isValidBody(commentData.body)) {
+      return false
+    }
+
+    const query = `
+      insert into comment
+      (post_id, user_id, body)
+      values (?, ?, ?)
+    `
+    const values = [commentData.postId, commentData.userId, commentData.body]
+    const [err] = await Db.query(query, values)
+
+    if (err) {
+      console.error(err.message)
+      return false
+    }
+
+    return true
+  }
 }
