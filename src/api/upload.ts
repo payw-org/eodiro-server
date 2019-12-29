29420805a1de9fd7786071a9d1f10812bad4027f
Jang Haemin <io@jhaemin.com>
2019-12-29 13:31:18 +0900

    feat: uploading files under 10MB

--- src/api/upload.ts
+++ src/api/upload.ts
@@ -4,37 +4,58 @@ import Config from '@@/config'
 import uuidv4 from 'uuid/v4'
 import mime from 'mime'
 import fs from 'fs'
+import SqlB from '@/modules/sqlb'
+import Db from '@/db'
 
 const router = express.Router()
-const upload = multer({
-  limits: {
-    fileSize: 1024 * 1024 * 5
-  }
-}).single('file')
+const upload = multer().array('file')
+const sqlb = SqlB()
 
 router.post('/upload', (req, res) => {
-  upload(req, res, (err) => {
-    if (err) {
-      console.error(err)
-
-      res.json({
-        err: 'fileTooLarge'
-      })
-
-      return
-    }
-
+  upload(req, res, async (err) => {
     const storagePath =
       process.env.NODE_ENV === 'development'
         ? Config.STORAGE_PATH_DEV
         : Config.STORAGE_PATH
 
-    const file = req.file
-    const fileName = uuidv4()
-    const fileExtension = mime.getExtension(file.mimetype)
-    const buffer = file.buffer
+    const files = Array.from(req.files as Express.Multer.File[])
+    const uuids = []
 
-    fs.writeFileSync(`${storagePath}/${fileName}.${fileExtension}`, buffer)
+    for (let i = 0; i < files.length; i++) {
+      const file = files[i]
+      const originalName = file.originalname
+      const uuid = uuidv4()
+      const mimeType = file.mimetype
+      const fileExtension = mime.getExtension(mimeType)
+      const buffer = file.buffer
+
+      if (file.size > 1024 * 1024 * 5) {
+        res.status(200).json({
+          err: 'fileTooLarge'
+        })
+        return
+      }
+
+      try {
+        fs.mkdirSync(`${storagePath}/${uuid}`)
+      } catch (error) {
+        res.sendStatus(500)
+        return
+      }
+
+      try {
+        fs.writeFileSync(`${storagePath}/${uuid}/${originalName}`, buffer)
+      } catch (error) {
+        res.sendStatus(500)
+        return
+      }
+
+      uuids.push(uuid)
+    }
+
+    res.status(200).json({
+      uuids
+    })
   })
 })
 
