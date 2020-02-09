6d26c78ea4ad653d19d6df3442b8b661f5b079ff
Jang Haemin <io@jhaemin.com>
2020-02-09 18:48:41 -0500

    chore: say hello to entry

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,11 +2,20 @@ import express from 'express'
 import peperoSquareRouter from '@/api/pepero-square'
 import authRouter from '@/api/auth'
 import uploadRouter from '@/api/upload'
+import lecturesRouter from '@/api/lectures'
 
 const router = express.Router()
 
+router.get('/', (...ctx) => {
+  const res = ctx[1]
+  res.send(`<pre>
+eodiro API 2
+<a href="https://github.com/paywteam/eodiro-api2">Documentation</a>
+</pre>`)
+})
 router.use('/pepero-square', peperoSquareRouter)
 router.use('/auth', authRouter)
 router.use(uploadRouter)
+router.use(lecturesRouter)
 
 export default router
