4155a54a0f76aa0c671992152c60bcfb37e58916
Jang Haemin <io@jhaemin.com>
2020-02-10 21:16:18 -0500

    feat: add more options for searching lectures

--- src/api/lectures/index.ts
+++ src/api/lectures/index.ts
@@ -23,42 +23,6 @@ const lectureCols = [
   'note',
 ]
 
-router.get('/lectures/search', async (req, res) => {
-  const searchKeyword = req.query.q
-  const amount = req.query.amount || 20
-  const offset = req.query.offset || 0
-
-  const [err, results] = await Db.query(
-    SqlB()
-      .select(...lectureCols)
-      .from('lecture')
-      .where()
-      .like('college', `%${searchKeyword}%`)
-      .or()
-      .like('major', `%${searchKeyword}%`)
-      .or()
-      .like('name', `%${searchKeyword}%`)
-      .or()
-      .like('schedule', `%${searchKeyword}%`)
-      .or()
-      .like('professor', `%${searchKeyword}%`)
-      .multiOrder([
-        ['name', 'ASC'],
-        ['professor', 'ASC'],
-        ['schedule', 'ASC'],
-      ])
-      .limit(amount, offset)
-      .build()
-  )
-
-  if (err) {
-    res.sendStatus(500)
-    return
-  }
-
-  res.json(results)
-})
-
 router.get('/:year/:semester/:campus/lectures', async (req, res) => {
   const { year, semester, campus } = req.params
   const amount = req.query.amount || 20
@@ -76,6 +40,44 @@ router.get('/:year/:semester/:campus/lectures', async (req, res) => {
     .limit(amount, offset)
     .build()
   const [err, results] = await Db.query(query)
+
+  if (err) {
+    res.sendStatus(500)
+    return
+  }
+
+  res.json(results)
+})
+
+router.get('/:year/:semester/:campus/lectures/search', async (req, res) => {
+  const { year, semester, campus } = req.params
+  const searchKeyword = req.query.q
+  const amount = req.query.amount || 20
+  const offset = req.query.offset || 0
+
+  const [err, results] = await Db.query(
+    SqlB()
+      .select(...lectureCols)
+      .from('lecture')
+      .where(`year=${year} AND semester='${semester}' AND campus='${campus}'`)
+      .and()
+      .like('college', `%${searchKeyword}%`)
+      .or()
+      .like('major', `%${searchKeyword}%`)
+      .or()
+      .like('name', `%${searchKeyword}%`)
+      .or()
+      .like('schedule', `%${searchKeyword}%`)
+      .or()
+      .like('professor', `%${searchKeyword}%`)
+      .multiOrder([
+        ['name', 'ASC'],
+        ['professor', 'ASC'],
+        ['schedule', 'ASC'],
+      ])
+      .limit(amount, offset)
+      .build()
+  )
 
   if (err) {
     res.sendStatus(500)
