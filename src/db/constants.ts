a7243e4e47307c197b9974f3aa223c427c5958b0
Jang Haemin <io@jhaemin.com>
2020-03-01 05:05:20 +0900

    chore: create db tables collections constants

---
+++ src/db/constants.ts
@@ -0,0 +1,11 @@
+export const DbTables = {
+  CAFETERIA_MENU: 'cafeteria_menu',
+  LECTURE: 'lecture',
+  PERIOD: 'period',
+  COVERAGE_COLLEGE: 'coverage_college',
+  COVERAGE_MAJOR: 'coverage_major',
+  COVERAGE_MAJOR_LECTURE: 'coverage_major_lecture',
+  POST: 'post',
+  USER: 'user',
+  PENDING_USER: 'pending_user',
+}
