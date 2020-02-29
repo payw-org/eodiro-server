0b199b98605672f925a8cf06d8bea8567cd89d16
Jang Haemin <io@jhaemin.com>
2020-03-01 05:05:25 +0900

    fix: typo

--- src/modules/convert-campus-name.ts
+++ src/modules/convert-campus-name.ts
@@ -1,6 +1,6 @@
 import { Campus } from '@/types'
 
-export default function converCampusName(campus: string): Campus {
+export default function convertCampusName(campus: string): Campus {
   campus = campus.toLowerCase()
   return campus === 'seoul' ? '서울' : campus === 'anseong' ? '안성' : '서울'
 }
