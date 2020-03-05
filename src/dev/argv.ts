00e8d47bae63262005df019f04b79dd01c989041
Jang Haemin <io@jhaemin.com>
2020-03-05 15:11:00 +0900

    feat: create a simple module that parse argv

---
+++ src/dev/argv.ts
@@ -0,0 +1,20 @@
+export default function argv<T = {}>(): T {
+  const args = {}
+  const rawArgs = process.argv.slice(2, process.argv.length)
+  rawArgs.forEach((arg: string, index) => {
+    // Long arguments with '--' flags:
+    if (arg.slice(0, 2).includes('--')) {
+      const longArgKey = arg.slice(2, arg.length)
+      const longArgValue = rawArgs[index + 1] // Next value, e.g.: --connection connection_name
+      args[longArgKey] = longArgValue
+    }
+    // Short arguments with '-' flags:
+    else if (arg.slice(0, 1).includes('-')) {
+      const longArgKey = arg.slice(1, arg.length)
+      const longArgValue = rawArgs[index + 1] // Next value, e.g.: -c connection_name
+      args[longArgKey] = longArgValue
+    }
+  })
+
+  return args as T
+}
