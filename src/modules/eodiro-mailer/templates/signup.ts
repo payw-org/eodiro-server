a96cefbaa04e4afaa194d20276eda7fde0da1e98
Jang Haemin <io@jhaemin.com>
2020-03-12 00:53:53 +0900

    feat: send a newly designed sign up verification email

--- src/modules/eodiro-mailer/templates/signup.ts
+++
@@ -1,9 +0,0 @@
-/**
- * @param {string} verificationCode
- */
-export default function(verificationCode: string): string {
-  const template = `
-    <a href="https://eodiro.com/verification?token=${verificationCode}">인증하기</a>
-  `
-  return template
-}
