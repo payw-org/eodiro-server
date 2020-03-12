3874eadc2a824eb3324175e600ff98aca4085041
Jang Haemin <io@jhaemin.com>
2020-03-12 21:36:35 +0900

    config(babel): convert to js config due to parse error

---
+++ babel.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  presets: ['@babel/preset-env', '@babel/typescript'],
+  plugins: [
+    '@babel/proposal-class-properties',
+    '@babel/plugin-transform-runtime',
+    'transform-html-import-to-string',
+    [
+      'module-resolver',
+      {
+        'root': ['./'],
+        'alias': {
+          '@': './src',
+        },
+      },
+    ],
+  ],
+  ignore: ['**/*.d.ts'],
+}
