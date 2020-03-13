0fca99436fd72137569a96066253762003b310ab
Jang Haemin <io@jhaemin.com>
2020-03-13 16:49:12 +0900

    refactor: make the prettier config module

---
+++ .prettier.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  parser: 'babel',
+  singleQuote: true,
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  bracketSpacing: true,
+  arrowParens: 'always',
+  htmlWhitespaceSensitivity: 'ignore',
+  quoteProps: 'preserve',
+  trailingComma: 'es5',
+}
