669644159c7d8f2d3739e08b7f6e45a0d3a0eb30
Jang Haemin <io@jhaemin.com>
2020-03-14 11:28:44 +0900

    feat: support generic type in ArrayUtil

--- src/modules/utils/array-util.ts
+++ src/modules/utils/array-util.ts
@@ -1,14 +1,14 @@
 export class ArrayUtil {
-  static remove(arr: any[], value: any): any[] {
+  static remove<T = any>(arr: T[], value: T): T[] {
     return arr.filter((v) => v !== value)
   }
 
-  static replace(arr: any[], value: any, newValue: any): any[] {
+  static replace<T = any>(arr: T[], value: T, newValue: T): T[] {
     const index = arr.indexOf(value)
     return index !== -1 ? arr.splice(index, 1, newValue) : arr
   }
 
-  static has(arr: any[], value: any): boolean {
+  static has<T = any>(arr: T[], value: T): boolean {
     return arr.indexOf(value) !== -1
   }
 }
