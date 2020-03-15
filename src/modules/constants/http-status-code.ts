5f9db5af7d1dd37ef825a322c47e90909134f077
Jang Haemin <io@jhaemin.com>
2020-03-15 22:50:05 +0900

    types: http status code

--- src/modules/constants/http-status-code.ts
+++ src/modules/constants/http-status-code.ts
@@ -1,9 +1,11 @@
+export type HTTPStatus = 200 | 201 | 204 | 400 | 401 | 403 | 500
+
 export const HttpStatusCode = {
-  OK: 200,
-  CREATED: 201,
-  NO_CONTENT: 204,
-  BAD_REQUEST: 400,
-  UNAUTHORIZED: 401,
-  FORBIDDEN: 403,
-  INTERNAL_SERVER_ERROR: 500,
+  OK: 200 as HTTPStatus,
+  CREATED: 201 as HTTPStatus,
+  NO_CONTENT: 204 as HTTPStatus,
+  BAD_REQUEST: 400 as HTTPStatus,
+  UNAUTHORIZED: 401 as HTTPStatus,
+  FORBIDDEN: 403 as HTTPStatus,
+  INTERNAL_SERVER_ERROR: 500 as HTTPStatus,
 }
