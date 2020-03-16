45ad7f1ed501e40045edab2fdc2bd53a789af611
Jang Haemin <io@jhaemin.com>
2020-03-16 14:57:25 +0900

    chore: remove the coverage college table

--- src/db/schema/create/coverage_major.js
+++ src/db/schema/create/coverage_major.js
@@ -2,8 +2,6 @@ module.exports = `
 CREATE TABLE coverage_major (
   name varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
   coverage_college varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  PRIMARY KEY (name),
-  KEY fk_coverage_college (coverage_college),
-  CONSTRAINT fk_coverage_college FOREIGN KEY (coverage_college) REFERENCES coverage_college (name) ON DELETE RESTRICT ON UPDATE CASCADE
+  PRIMARY KEY (name)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 `
