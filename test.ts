e7479b2811b00e07680dfd5f378ad2f2db8974bc
Jang Haemin <io@jhaemin.com>
2020-03-16 10:18:46 +0900

    chore: remove old test file

--- test.ts
+++
@@ -1,8 +0,0 @@
-import niceware from 'niceware'
-
-const passwdArr: string[] = niceware.generatePassphrase(6).map((word) => {
-  return word.charAt(0) + word.slice(1)
-})
-
-const passwdStr = passwdArr.join('-')
-console.log(passwdStr)
