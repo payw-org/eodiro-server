ba5f6fa1561b10d4951ad5651860490d69b85875
Jang Haemin <io@jhaemin.com>
2020-03-17 18:06:07 +0900

    fix: require authentication

--- src/api/eodiro-one-api/scheme/square/get-comments.action/function.ts
+++ src/api/eodiro-one-api/scheme/square/get-comments.action/function.ts
@@ -1,11 +1,19 @@
 import Db from '@/db'
 import { Comment, Comments } from '@/db/schema/generated'
+import Auth from '@/modules/auth'
 import SqlB from '@/modules/sqlb'
 import { Interface } from './interface'
 
 export default async function(
   data: Interface['data']
 ): Promise<Interface['payload']> {
+  const authPayload = await Auth.isSignedUser(data.accessToken)
+  if (!authPayload)
+    return {
+      err: 'Unauthorized',
+      data: null,
+    }
+
   const query = SqlB<Comment>()
     .select('*')
     .from('comment')
