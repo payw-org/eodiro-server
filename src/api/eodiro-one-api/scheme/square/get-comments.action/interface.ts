6cad85ad50881370cc39d69cf30d9785fd25b379
Jang Haemin <io@jhaemin.com>
2020-03-17 18:06:07 +0900

    fix: require authentication

--- src/api/eodiro-one-api/scheme/square/get-comments.action/interface.ts
+++ src/api/eodiro-one-api/scheme/square/get-comments.action/interface.ts
@@ -3,6 +3,7 @@ import { OneAPIError } from '../../types/utils'
 
 export interface Interface {
   data: {
+    accessToken: string
     postId: number
     mostRecentCommentId?: number
     amount?: number
