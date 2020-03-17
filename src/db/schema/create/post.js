a5d085f26f265bf204bf69da6520a628acb03c4e
Jang Haemin <io@jhaemin.com>
2020-03-17 23:09:18 +0900

    feat: board table

--- src/db/schema/create/post.js
+++ src/db/schema/create/post.js
@@ -1,6 +1,7 @@
 module.exports = `
 CREATE TABLE post (
   id int NOT NULL AUTO_INCREMENT,
+  board_id int NOT NULL,
   title varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
   body text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   user_id int NOT NULL,
@@ -10,6 +11,8 @@ CREATE TABLE post (
   random_nickname varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
   PRIMARY KEY (id),
   KEY user_id_idx (user_id),
+  KEY fk_post_board_id (board_id),
+  CONSTRAINT fk_post_board_id FOREIGN KEY (board_id) REFERENCES board (id) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT fk_post_user_id FOREIGN KEY (user_id) REFERENCES user (id) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=169 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=228 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 `
