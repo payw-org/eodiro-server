be08b17c7420e19e907b1f6cc8244a6a26fea8e1
Jang Haemin <io@jhaemin.com>
2020-03-29 21:44:18 +0900

    chore: log the whole error

--- src/db/index.ts
+++ src/db/index.ts
@@ -54,7 +54,7 @@ export default class Db {
       conn.query(query, values, (err, results, fields) => {
         if (err) {
           console.log('Query: ', query)
-          console.error(err.sqlMessage)
+          console.error(err)
           throw err
         }
         resolve([err, results, fields])
