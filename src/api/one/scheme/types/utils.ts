caee4a7360cefc0a5f56356e7595a0de8715e8aa
Jang Haemin <io@jhaemin.com>
2020-03-30 22:52:35 +0900

    feat: add some new utility types

--- src/api/one/scheme/types/utils.ts
+++ src/api/one/scheme/types/utils.ts
@@ -1,20 +1,26 @@
-import { APIScheme } from '..'
+import { OneApiAction } from '..'
 
+/**
+ * Where the API requires the authentication
+ */
 export type AuthRequired<T> = T & {
   accessToken: string
 }
 
-export type OneAPIData<T extends APIScheme> = T['data']
-export type OneAPIPayload<T extends APIScheme> = T['payload']
+/**
+ * The basic form of One API action's interface
+ */
+interface OneApiActionSkeleton {
+  data: unknown
+  payload: unknown
+}
 
-export type OneAPIError<T> =
-  | null
-  | 'Unauthorized'
-  | 'No Content'
-  | 'Bad Request'
-  | 'Forbidden'
-  | 'Internal Server Error'
-  | T
+export type OneApiData<T extends OneApiActionSkeleton> = T['data']
+export type OneApiPayload<T extends OneApiActionSkeleton> = T['payload']
+
+export type OneApiFunction<T extends OneApiActionSkeleton> = (
+  data: OneApiData<T>
+) => Promise<OneApiPayload<T>>
 
 export enum OneApiError {
   UNAUTHORIZED = 'Unauthorized',
@@ -23,3 +29,24 @@ export enum OneApiError {
   INTERNAL_SERVER_ERROR = 'Internal Server Error',
   NO_CONTENT = 'No Content',
 }
+
+/**
+ * @deprecated Use `OneApiData` instead
+ */
+export type OneAPIData<T extends OneApiAction> = T['data']
+/**
+ * @deprecated Use `OneApiPayload` instead
+ */
+export type OneAPIPayload<T extends OneApiAction> = T['payload']
+
+/**
+ * @deprecated Use `OneApiError`
+ */
+export type OneAPIError<T> =
+  | null
+  | 'Unauthorized'
+  | 'No Content'
+  | 'Bad Request'
+  | 'Forbidden'
+  | 'Internal Server Error'
+  | T
