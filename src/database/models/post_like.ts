b4766b6a8be1a5ba6b2623ad4d86d394b91f7ebc
Jang Haemin <io@jhaemin.com>
2020-04-04 11:51:25 +0900

    feat: merge table info into model class itself

--- src/database/models/post_like.ts
+++ src/database/models/post_like.ts
@@ -1,39 +1,40 @@
 import { DataTypes, Model } from 'sequelize'
 import { createInitModelFunction } from '../create-init-model'
-import { TableInfo } from '../types'
 
-export class PostLike extends Model {}
+export class PostLike extends Model {
+  static tableName = 'post_like'
+  static attrs = {
+    user_id: 'user_id',
+    post_id: 'post_id',
+  }
+}
 
 /**
  * Init `PostLike` model then return it
  */
-export const initPostLike = createInitModelFunction(PostLike, 'post_like', {
-  user_id: {
-    primaryKey: true,
-    type: DataTypes.INTEGER,
-    references: {
-      model: 'user',
-      key: 'id',
+export const initPostLike = createInitModelFunction(
+  PostLike,
+  PostLike.tableName,
+  {
+    user_id: {
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+      onDelete: 'cascade',
+      onUpdate: 'cascade',
     },
-    onDelete: 'cascade',
-    onUpdate: 'cascade',
-  },
-  post_id: {
-    primaryKey: true,
-    type: DataTypes.INTEGER,
-    references: {
-      model: 'post',
-      key: 'id',
+    post_id: {
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'post',
+        key: 'id',
+      },
+      onDelete: 'cascade',
+      onUpdate: 'cascade',
     },
-    onDelete: 'cascade',
-    onUpdate: 'cascade',
-  },
-})
-
-export const $PostLike: TableInfo = {
-  tableName: 'post_like',
-  attrs: {
-    user_id: 'user_id',
-    post_id: 'post_id',
-  },
-}
+  }
+)
--- src/database/types/index.ts
+++
@@ -1,4 +0,0 @@
-export interface TableInfo {
-  tableName: string
-  attrs: object
-}
