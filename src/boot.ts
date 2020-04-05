a127ce0de6dc4f4cf09609dd58c8cf85873a7563
Jang Haemin <io@jhaemin.com>
2020-04-05 18:39:59 +0900

    chore: remove headers option

--- src/boot.ts
+++ src/boot.ts
@@ -37,13 +37,6 @@ export async function boot(options: {
         'https://api2.eodiro.com',
         'https://eodiro.com',
       ],
-      allowedHeaders: [
-        'Accept',
-        'Authorization',
-        'Content-Type',
-        'AccessToken',
-      ],
-      credentials: true,
     })
   )
 
