cc0680ecc5b9c08acf20584cbf14e9ce27016a24
Jang Haemin <io@jhaemin.com>
2020-04-05 18:22:17 +0900

    fix: cors (multipart/form-data)

--- src/boot.ts
+++ src/boot.ts
@@ -37,6 +37,7 @@ export async function boot(options: {
         'https://api2.eodiro.com',
         'https://eodiro.com',
       ],
+      allowedHeaders: ['Accept', 'Authorization', 'Content-Type'],
       credentials: true,
     })
   )
