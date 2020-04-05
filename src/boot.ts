335994cef9a5357b7834449255816bb6a0e81bb7
Jang Haemin <io@jhaemin.com>
2020-04-05 23:09:12 +0900

    chore: expand the limit

--- src/boot.ts
+++ src/boot.ts
@@ -32,8 +32,8 @@ export async function boot(options: {
   app.use(cors())
 
   // Use middlewares
-  app.use(bodyParser.json({ limit: '50mb' }))
-  app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }))
+  app.use(bodyParser.json({ limit: '100mb' }))
+  app.use(bodyParser.urlencoded({ limit: '100mb', extended: true }))
   app.use(api)
 
   if (db) {
