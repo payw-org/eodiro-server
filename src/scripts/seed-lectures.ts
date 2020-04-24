f1158a283105b0ad1c8b16407e67f31ce98d02e6
jhaemin <io@jhaemin.com>
2020-04-24 13:29:27 +0900

    chore: move directory

--- src/scripts/seed-lectures.ts
+++ src/scripts/seed-lectures.ts
@@ -1,14 +1,16 @@
-import { boot } from '@/boot'
-import Config from '@/config'
-import collegesMajorsSeeder from '@/db/seeders/colleges-majors-seeder'
-import timetableSeeder from '@/db/seeders/timetable-seeder'
-import argv from '@/dev/argv'
-import getSemester from '@/modules/get-semester'
-import { Semester } from '@/types'
 import { CTTS } from '@payw/cau-timetable-scraper'
+import Config from '@/config'
+import { Semester } from '@/types'
+import argv from '@/dev/argv'
+import { boot } from '@/boot'
 import chalk from 'chalk'
+import collegesMajorsSeeder from '@/db/seeders/colleges-majors-seeder'
 import dayjs from 'dayjs'
 import fs from 'fs'
+import getSemester from '@/modules/get-semester'
+import timetableSeeder from '@/db/seeders/timetable-seeder'
+
+const log = console.log
 
 async function main(): Promise<void> {
   const args = argv<{
@@ -29,12 +31,10 @@ async function main(): Promise<void> {
   const semester =
     (args.semester as Semester) || (args.s as Semester) || getSemester()
 
-  console.log(
-    `[ ${chalk.blue('seeding')} ] seeding lectures: ${year}, ${semester}`
-  )
+  log(`[ ${chalk.blue('seeding')} ] seeding lectures: ${year}, ${semester}`)
 
   if (!args.collegesFile && !args.lecturesFile) {
-    console.log('Scraping from server')
+    log('Scraping from server')
     const { colleges, lectures } = await CTTS(
       {
         id: Config.CAU_ID,
@@ -61,18 +61,18 @@ async function main(): Promise<void> {
     await timetableSeeder(lectures)
   } else {
     if (args.collegesFile) {
-      console.log('Colleges file is given')
+      log('Colleges file is given')
       const colleges = JSON.parse(fs.readFileSync(args.collegesFile, 'utf8'))
       await collegesMajorsSeeder(colleges)
     }
     if (args.lecturesFile) {
-      console.log('Lectures file is given')
+      log('Lectures file is given')
       const lectures = JSON.parse(fs.readFileSync(args.lecturesFile, 'utf8'))
       await timetableSeeder(lectures)
     }
   }
 
-  console.log(`[ ${chalk.blue('seeding')} ] Done seeding lectures`)
+  log(`[ ${chalk.blue('seeding')} ] Done seeding lectures`)
 }
 
 main()
