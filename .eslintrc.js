e43efe3fa2e1200c41957bc9cafc2f1afee202cc
Jang Haemin <io@jhaemin.com>
2020-05-10 20:49:24 +0900

    config: integrate prettier to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,9 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   extends: [
-    'eslint:recommended',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
   ],
   parserOptions: {
     ecmaVersion: 2018,
