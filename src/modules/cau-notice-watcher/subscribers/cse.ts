ea218156b10bed651629351ecfd8ad25b2dd5186
Jang Haemin <io@jhaemin.com>
2020-05-19 10:18:42 +0900

    feat(notice): add cse support

--- src/modules/cau-notice-watcher/subscribers/cse.ts
+++ src/modules/cau-notice-watcher/subscribers/cse.ts
@@ -1,15 +1,10 @@
 import { Subscriber } from '..'
 
 export const cau: Subscriber = {
-  name: 'CAU Notice',
+  name: '소프트웨어학부',
   key: 'cau',
   link: 'https://cse.cau.ac.kr/sub05/sub0501.php',
   noticeItemSelector: '.table-basic tbody tr',
-  titleBuilder: (noticeElm) => {
-    // const mark = noticeElm.querySelector('.mark_noti').textContent.trim()
-    // const title = noticeElm.querySelector('a').textContent.trim()
-    // return `${mark} ${title}`
-
-    return ''
-  },
+  titleBuilder: (noticeElm) =>
+    noticeElm.querySelector('a').textContent.trim().replace(/NEW$/, '').trim(),
 }
