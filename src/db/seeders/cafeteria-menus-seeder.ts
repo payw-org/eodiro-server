d865b358604901ba02bf041ea3c9c61d3668dcac
jhaemin <io@jhaemin.com>
2020-05-21 21:56:08 +0900

    feat(eodiro-bot): inform scrape failure

--- src/db/seeders/cafeteria-menus-seeder.ts
+++ src/db/seeders/cafeteria-menus-seeder.ts
@@ -15,74 +15,83 @@ const CafeteriaMenusSeeder = (): void => {
  * Seed 5 days of cafeteria menus starting from today
  */
 CafeteriaMenusSeeder.seed = async (): Promise<void> => {
-  console.log(`🌱 Seeding cafeteria menus...`)
+  try {
+    console.log(`🌱 Seeding cafeteria menus...`)
 
-  const menus = await CCMS({
-    id: Config.CAU_ID,
-    pw: Config.CAU_PW,
-    days: 5,
-  })
-  const dbCafeteriaMenus: CafeteriaMenuType[] = []
-  const daysList: string[] = []
-
-  menus.days.forEach((day) => {
-    daysList.push(day.date)
-    dbCafeteriaMenus.push({
-      campus: convertCampusName(menus.campus),
-      served_at: day.date,
-      data: JSON.stringify({
-        breakfast: day.breakfast,
-        lunch: day.lunch,
-        supper: day.supper,
-      }),
+    const menus = await CCMS({
+      id: Config.CAU_ID,
+      pw: Config.CAU_PW,
+      days: 5,
     })
-  })
 
-  const query = SqlB()
-    .delete()
-    .from('cafeteria_menu')
-    .where(
-      daysList
-        .map((day) => {
-          return `campus = '${convertCampusName(
-            menus.campus
-          )}' AND served_at = '${day}'`
-        })
-        .join(' OR ')
-    )
-    .build()
-  const [err] = await Db.query(query)
+    const dbCafeteriaMenus: CafeteriaMenuType[] = []
+    const daysList: string[] = []
 
-  if (err) {
-    console.log(`🌱 Seeding failed`)
-    EodiroMailer.sendMail({
-      subject: 'Failed to seed cafeteria menus',
-      to: 'contact@payw.org',
-      html: `
+    menus.days.forEach((day) => {
+      daysList.push(day.date)
+      dbCafeteriaMenus.push({
+        campus: convertCampusName(menus.campus),
+        served_at: day.date,
+        data: JSON.stringify({
+          breakfast: day.breakfast,
+          lunch: day.lunch,
+          supper: day.supper,
+        }),
+      })
+    })
+
+    const query = SqlB()
+      .delete()
+      .from('cafeteria_menu')
+      .where(
+        daysList
+          .map((day) => {
+            return `campus = '${convertCampusName(
+              menus.campus
+            )}' AND served_at = '${day}'`
+          })
+          .join(' OR ')
+      )
+      .build()
+    const [err] = await Db.query(query)
+
+    if (err) {
+      console.log(`🌱 Seeding failed`)
+      EodiroMailer.sendMail({
+        subject: 'Failed to seed cafeteria menus',
+        to: 'contact@payw.org',
+        html: `
 query: ${query}
 err: ${err}
 `,
+      })
+      return
+    }
+
+    const [err2] = await Db.query(
+      SqlB().bulkInsert(TableNames.cafeteria_menu, dbCafeteriaMenus).build()
+    )
+
+    if (err2) {
+      console.log(`🌱 Seeding failed`)
+      return
+    }
+
+    console.log(`🌱 Successfully seeded 5 days of cafeteria menus`)
+
+    // Send an email after complete
+    EodiroMailer.sendMail({
+      subject: '[eodiro Bot] Scraped cafeteria menus',
+      to: 'io@jhaemin.com',
+      html: JSON.stringify(dbCafeteriaMenus),
     })
-    return
+  } catch (error) {
+    // Send an email when failed
+    EodiroMailer.sendMail({
+      subject: '[eodiro Bot] Failed to scrape cafeteria menus',
+      to: 'io@jhaemin.com',
+    })
   }
-
-  const [err2] = await Db.query(
-    SqlB().bulkInsert(TableNames.cafeteria_menu, dbCafeteriaMenus).build()
-  )
-
-  if (err2) {
-    console.log(`🌱 Seeding failed`)
-    return
-  }
-
-  console.log(`🌱 Successfully seeded 5 days of cafeteria menus`)
-
-  // Send an email after complete
-  EodiroMailer.sendMail({
-    subject: '[eodiro Bot] Scraped cafeteria menus',
-    to: 'io@jhaemin.com',
-    html: JSON.stringify(dbCafeteriaMenus),
-  })
 }
 
 export default CafeteriaMenusSeeder
