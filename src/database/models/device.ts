3223def3314e62b98b5f0465dc2333ee534f542e
jhaemin <io@jhaemin.com>
2020-05-22 09:05:34 +0900

    chore(db): singularize the device table name

--- src/database/models/device.ts
+++ src/database/models/device.ts
@@ -3,9 +3,9 @@ import { DataTypes, Model } from 'sequelize'
 import { PrimaryAIAttribute } from '../utils/model-attributes'
 import { createInitModelFunction } from '../create-init-model'
 
-class Devices extends Model {}
+class Device extends Model {}
 
-export const getDevices = createInitModelFunction(Devices, 'devices', {
+export const getDevice = createInitModelFunction(Device, 'device', {
   id: PrimaryAIAttribute,
   user_id: {
     type: DataTypes.INTEGER,
