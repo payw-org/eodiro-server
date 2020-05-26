8898583bc8c54369f87ba967501d21f9be183cbe
jhaemin <io@jhaemin.com>
2020-05-26 18:15:13 +0900

    Fix #41 - Replace Zombie.js with Puppeteer - Change `link` to `url` - Remove class properties - Add some documentations

--- src/modules/cau-notice-watcher/subscribers/cau.ts
+++ src/modules/cau-notice-watcher/subscribers/cau.ts
@@ -3,7 +3,7 @@ import { Subscriber } from '..'
 export const cau: Subscriber = {
   name: 'CAU Notice',
   key: 'cau',
-  link: 'https://www.cau.ac.kr/cms/FR_CON/index.do?MENU_ID=100#page1',
+  url: 'https://www.cau.ac.kr/cms/FR_CON/index.do?MENU_ID=100#page1',
   noticeItemSelector: '.typeNoti',
   titleBuilder: (noticeElm) => {
     const mark = noticeElm.querySelector('.mark_noti').textContent.trim()
