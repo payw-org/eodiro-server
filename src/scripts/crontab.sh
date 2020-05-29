c3381b12abf95876f4d5613d555e8f436fb1725c
jhaemin <io@jhaemin.com>
2020-05-29 13:13:57 +0900

    feat: update cron

--- src/scripts/crontab.sh
+++ src/scripts/crontab.sh
@@ -1,5 +1,14 @@
 # run `crontab -e` to edit crontab
 # copy and paste the crons below
 
-0 0 * * * NODE_ENV=development /home/ubuntu/.nvm/versions/node/v13.14.0/bin/node /home/ubuntu/eodiro/server/build/src/scripts/update-random-nickname
-0 3 * * * NODE_ENV=development /home/ubuntu/.nvm/versions/node/v13.14.0/bin/node /home/ubuntu/eodiro/server/build/src/scripts/seed-cafeteria-menus
+# update random nicknames at 00:00
+0 0 * * * NODE_ENV=production /home/ubuntu/.nvm/versions/node/v13.14.0/bin/node /home/ubuntu/eodiro/server/build/src/scripts/update-random-nickname
+
+# seed cafeteria menus at 03:00 everyday
+0 3 * * * NODE_ENV=production /home/ubuntu/.nvm/versions/node/v13.14.0/bin/node /home/ubuntu/eodiro/server/build/src/scripts/seed-cafeteria-menus
+
+# garbage collect files every 3 hours
+0 */3 * * * NODE_ENV=production /home/ubuntu/.nvm/versions/node/v13.14.0/bin/node /home/ubuntu/eodiro/server/build/src/scripts/garbage-collect-files
+
+# clear pending users every 30 minutes
+*/30 * * * * NODE_ENV=production /home/ubuntu/.nvm/versions/node/v13.14.0/bin/node /home/ubuntu/eodiro/server/build/src/scripts/clear-pending-users
