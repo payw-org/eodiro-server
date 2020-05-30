ba678b81579bd886558d4e1b8ec5485d0508a64a
jhaemin <io@jhaemin.com>
2020-05-30 19:03:51 +0900

    fix: wrong post id

--- src/api/one/scheme/my/get-my-posts.action/function.ts
+++ src/api/one/scheme/my/get-my-posts.action/function.ts
@@ -1,9 +1,9 @@
-import { eodiroQuery } from '@/database/eodiro-query'
+import { Action } from './interface'
 import { BoardType } from '@/database/models/board'
+import { OneApiFunction } from '../../types/utils'
 import { PostAttrs } from '@/database/models/post'
 import { Q } from '@/modules/sqlb'
-import { OneApiFunction } from '../../types/utils'
-import { Action } from './interface'
+import { eodiroQuery } from '@/database/eodiro-query'
 
 const func: OneApiFunction<Action> = async (data) => {
   const { authPayload } = data
@@ -11,16 +11,18 @@ const func: OneApiFunction<Action> = async (data) => {
   const lastPostId = data.lastPostId || 2147483647
   const amount = data.amount || 20
 
-  const posts = await eodiroQuery<PostAttrs & BoardType>(
-    Q()
-      .select()
-      .from(Q().join('post', 'board').on('post.board_id = board.id'))
-      .where(
-        Q().equal('user_id', authPayload.userId).andLess('post.id', lastPostId)
-      )
-      .order('post.id', 'DESC')
-      .limit(amount)
-  )
+  const query = Q()
+    .select(
+      'post.id as id, board_id, title, body, user_id, random_nickname, uploaded_at, edited_at, is_deleted, board_name'
+    )
+    .from(Q().join('post', 'board').on('post.board_id = board.id'))
+    .where(
+      Q().equal('user_id', authPayload.userId).andLess('post.id', lastPostId)
+    )
+    .order('post.id', 'DESC')
+    .limit(amount)
+
+  const posts = await eodiroQuery<PostAttrs & BoardType>(query)
 
   return {
     err: null,
