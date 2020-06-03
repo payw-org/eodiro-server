9ea2589d6602ef8fc5630c75c9b9d2eecdc20a43
jhaemin <io@jhaemin.com>
2020-06-03 20:10:28 +0900

    doc(cron): add instruction for restarting the cron

--- src/scripts/crontab.sh
+++ src/scripts/crontab.sh
@@ -1,5 +1,9 @@
 # run `crontab -e` to edit crontab
 # copy and paste the crons below
+#
+# (optional) restart cron service when its timezone does not follow the system timezone
+# > sudo service cron stop
+# > sudo service cron start
 
 # update random nicknames at 00:00
 0 0 * * * NODE_ENV=production /home/ubuntu/.nvm/versions/node/v13.14.0/bin/node /home/ubuntu/eodiro/server/build/src/scripts/update-random-nicknames
