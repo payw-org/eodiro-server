1b799ac828af71f540b56937e94c94c15833b127
GardenLee <khjvvv7@gmail.com>
2020-06-12 00:10:29 +0900

    fix: add topic field to Create Tip Input

--- src/api/one/scheme/tip/create-tip.action/interface.ts
+++ src/api/one/scheme/tip/create-tip.action/interface.ts
@@ -1,8 +1,10 @@
 import { AuthRequired, OneApiError } from '@/api/one/scheme/types/utils'
 
+import { tip_topic } from '@prisma/client'
+
 export interface Action {
   data: AuthRequired<{
-    topic: string
+    topic: tip_topic
     title: string
     body: string
   }>
