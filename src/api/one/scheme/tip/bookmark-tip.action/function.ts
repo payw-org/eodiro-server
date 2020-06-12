c8f4b589597c50e3d1c0df2dd2cfb5d22b08b30e
GardenLee <khjvvv7@gmail.com>
2020-06-13 01:35:02 +0900

    refactor: bookmark tip api use Tip method

--- src/api/one/scheme/tip/bookmark-tip.action/function.ts
+++ src/api/one/scheme/tip/bookmark-tip.action/function.ts
@@ -1,23 +1,16 @@
 import { Action } from './interface'
 import { OneApiFunction } from '@/api/one/scheme/types/utils'
 import { Tip } from '@/database/models/tip'
-import prisma from '@/modules/prisma'
 
 const func: OneApiFunction<Action> = async (data) => {
   const { authPayload, tipId } = data
   const { userId } = authPayload
   let isBookmarked = false
 
-  const bookmark = await prisma.tipBookmark.findMany({
-    where: {
-      AND: [{ userId: userId }, { tipId: tipId }],
-    },
-  })
-
-  if (bookmark.length === 0) {
-    isBookmarked = await Tip.bookmark(userId, tipId)
-  } else {
+  if (await Tip.isBookmarked(userId, tipId)) {
     isBookmarked = await Tip.cancelBookmark(userId, tipId)
+  } else {
+    isBookmarked = await Tip.bookmark(userId, tipId)
   }
 
   return {
