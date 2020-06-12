44bb468f2335a9bcb87b9c4801110343354b9233
GardenLee <khjvvv7@gmail.com>
2020-06-13 01:26:17 +0900

    refactor: tip like api use Tip method

--- src/api/one/scheme/tip/like-tip.action/function.ts
+++ src/api/one/scheme/tip/like-tip.action/function.ts
@@ -1,23 +1,16 @@
 import { Action } from './interface'
 import { OneApiFunction } from '@/api/one/scheme/types/utils'
 import { Tip } from '@/database/models/tip'
-import prisma from '@/modules/prisma'
 
 const func: OneApiFunction<Action> = async (data) => {
   const { authPayload, tipId } = data
   const { userId } = authPayload
   let isLike = false
 
-  const like = await prisma.tipLike.findMany({
-    where: {
-      AND: [{ userId: userId }, { tipId: tipId }],
-    },
-  })
-
-  if (like.length === 0) {
-    isLike = await Tip.like(userId, tipId)
-  } else {
+  if (await Tip.isLiked(userId, tipId)) {
     isLike = await Tip.unlike(userId, tipId)
+  } else {
+    isLike = await Tip.like(userId, tipId)
   }
 
   return {
