85871a2da17a5d3ff5084381872427e33c0bb129
GardenLee <khjvvv7@gmail.com>
2020-06-13 23:48:23 +0900

    feat: add tip_comment table

---
+++ src/database/models/tip_comment.ts
@@ -0,0 +1,54 @@
+import { DataTypes, Model } from 'sequelize'
+
+import { PrimaryAIAttribute } from '../utils/model-attributes'
+import { createInitModelFunction } from '../create-init-model'
+
+export class TipComment extends Model {}
+
+export const getTipComment = createInitModelFunction(
+  TipComment,
+  'tip_comment',
+  {
+    id: PrimaryAIAttribute,
+    user_id: {
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+      onDelete: 'cascade',
+      onUpdate: 'cascade',
+    },
+    tip_id: {
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'tip',
+        key: 'id',
+      },
+      onDelete: 'cascade',
+      onUpdate: 'cascade',
+    },
+    body: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    random_nickname: {
+      type: DataTypes.STRING(30),
+      allowNull: false,
+    },
+    is_removed: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
+    created_at: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+    },
+    edited_at: {
+      type: DataTypes.DATE,
+      allowNull: true,
+    },
+  }
+)
