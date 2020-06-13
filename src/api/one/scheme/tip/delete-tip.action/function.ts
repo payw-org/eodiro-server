c7ba985b5284159a464155234d4e459c3d39f59f
GardenLee <khjvvv7@gmail.com>
2020-06-13 20:59:31 +0900

    refactor: delete tip api

--- src/api/one/scheme/tip/delete-tip.action/function.ts
+++ src/api/one/scheme/tip/delete-tip.action/function.ts
@@ -1,28 +1,22 @@
 import { OneApiError, OneApiFunction } from '@/api/one/scheme/types/utils'
 
 import { Action } from './interface'
-import { Tip } from '@prisma/client'
+import { Tip } from '@/database/models/tip'
 import prisma from '@/modules/prisma'
 
 const func: OneApiFunction<Action> = async (data) => {
   const { authPayload, tipId } = data
   const { userId } = authPayload
-  let tip: Tip
 
-  try {
-    tip = await prisma.tip.findOne({ where: { id: tipId } })
-  } catch (err) {
+  if ((await prisma.tip.findOne({ where: { id: tipId } })) === null) {
     return {
       err: OneApiError.NO_CONTENT,
       data: null,
     }
   }
 
-  if (tip.userId !== userId) {
-    return {
-      err: OneApiError.FORBIDDEN,
-      data: null,
-    }
+  if (!Tip.isOwnedBy(userId, tipId)) {
+    return { err: OneApiError.FORBIDDEN, data: null }
   }
 
   await prisma.tip.delete({ where: { id: tipId } })
