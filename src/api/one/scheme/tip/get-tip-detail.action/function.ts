777c667ba2965ce8e4211c7209447da920e4dd47
GardenLee <khjvvv7@gmail.com>
2020-06-16 21:17:36 +0900

    refactor: get tip detail api's req / res

--- src/api/one/scheme/tip/get-tip-detail.action/function.ts
+++ src/api/one/scheme/tip/get-tip-detail.action/function.ts
@@ -1,10 +1,11 @@
-import { OneApiError, OneApiFunction } from '@/api/one/types'
+import { OneApiError, OneApiFunc } from '@/api/one/types'
 import { Tip, TipResponse } from '@/database/models/tip'
 
 import { Action } from './interface'
+import { oneApiResponse } from '@/api/one/utils'
 import prisma from '@/modules/prisma'
 
-const func: OneApiFunction<Action> = async (data) => {
+const func: OneApiFunc<Action> = async (data) => {
   const { authPayload, tipId } = data
   const { userId } = authPayload
 
@@ -19,10 +20,7 @@ const func: OneApiFunction<Action> = async (data) => {
       },
     })
     if (tip === null) {
-      return {
-        err: OneApiError.NO_CONTENT,
-        data: null,
-      }
+      return oneApiResponse<Action>(OneApiError.NO_CONTENT)
     }
 
     const tipResponse: TipResponse = {
@@ -33,15 +31,9 @@ const func: OneApiFunction<Action> = async (data) => {
       isBookmarked: await Tip.isBookmarked(userId, tipId),
     }
 
-    return {
-      err: null,
-      data: tipResponse,
-    }
+    return oneApiResponse<Action>(tipResponse)
   } catch (err) {
-    return {
-      err: OneApiError.INTERNAL_SERVER_ERROR,
-      data: null,
-    }
+    return oneApiResponse<Action>(OneApiError.INTERNAL_SERVER_ERROR)
   }
 }
 
