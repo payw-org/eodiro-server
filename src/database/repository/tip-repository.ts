81c55aa039c27334c7a96d64d594513b808e30b6
GardenLee <khjvvv7@gmail.com>
2020-06-19 00:23:40 +0900

    refactor: get tips api uses TipRepository

--- src/database/repository/tip-repository.ts
+++ src/database/repository/tip-repository.ts
@@ -1,5 +1,6 @@
 import { TipAttrs, TipResponse } from '../models/tip'
 
+import { TipTopic } from '@prisma/client'
 import prisma from '@/modules/prisma'
 
 export class TipRepository {
@@ -14,5 +15,42 @@ export class TipRepository {
       },
     })
     return tip
+  }
+
+  static async findByTopic(
+    topic: TipTopic,
+    pageSize: number,
+    page: number
+  ): Promise<TipAttrs[]> {
+    const tipList = await prisma.tip.findMany({
+      where: {
+        isRemoved: false,
+        topic: topic,
+      },
+      include: {
+        tipLikes: true,
+        tipBookmarks: true,
+      },
+      take: -pageSize,
+      skip: (page - 1) * pageSize,
+      orderBy: { createdAt: 'desc' },
+    })
+    return tipList
+  }
+
+  static async findAll(pageSize: number, page: number): Promise<TipAttrs[]> {
+    const tipList = await prisma.tip.findMany({
+      where: {
+        isRemoved: false,
+      },
+      include: {
+        tipLikes: true,
+        tipBookmarks: true,
+      },
+      take: -pageSize,
+      skip: (page - 1) * pageSize,
+      orderBy: { createdAt: 'desc' },
+    })
+    return tipList
   }
 }
