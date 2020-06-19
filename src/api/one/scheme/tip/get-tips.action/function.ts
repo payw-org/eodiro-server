f04bce1fbeed4f04885c1dca1d9d2c25675f27a3
jhaemin <io@jhaemin.com>
2020-06-19 17:18:15 +0900

    feat: prisma time mod

--- src/api/one/scheme/tip/get-tips.action/function.ts
+++ src/api/one/scheme/tip/get-tips.action/function.ts
@@ -5,6 +5,7 @@ import { OneApiFunc } from '@/api/one/types'
 import { TipRepository } from '@/database/repository/tip-repository'
 import { oneApiResponse } from '@/api/one/utils'
 import prisma from '@/modules/prisma'
+import { prismaTimeMod } from '@/modules/time'
 
 const func: OneApiFunc<Action> = async (data) => {
   const { topic, page } = data
@@ -18,16 +19,18 @@ const func: OneApiFunc<Action> = async (data) => {
     tipList = await TipRepository.findByTopic(topic, pageSize, page)
   }
 
-  const tips = tipList.map((item) => {
-    const response: TipListResponse = {
-      ...item,
-      tipLikes: item.tipLikes.length,
-      tipBookmarks: item.tipBookmarks.length,
-      isLiked: true,
-      isBookmarked: true,
-    }
-    return response
-  })
+  const tips = prismaTimeMod(
+    tipList.map((item) => {
+      const response: TipListResponse = {
+        ...item,
+        tipLikes: item.tipLikes.length,
+        tipBookmarks: item.tipBookmarks.length,
+        isLiked: true,
+        isBookmarked: true,
+      }
+      return response
+    })
+  )
 
   const totalPage =
     totalCount % 10 === 0 ? totalCount / 10 : Math.floor(totalCount / 10) + 1
--- src/modules/time.ts
+++ src/modules/time.ts
@@ -11,8 +11,45 @@ export default class Time {
   /**
    * Returns an artificially generated ISO time string
    * for resolving wrong timestamp insertions in MySQL database
+   *
+   * @deprecated
    */
   static getIsoString(): string {
     return dayjs().format('YYYY-MM-DDTHH:mm:ss.000') + 'Z'
   }
+
+  static getPrismaCurrent(): Date {
+    return dayjs().add(9, 'hour').toDate()
+  }
+
+  static utcToKst<T>(value: T): T {
+    function isPrimitive(test) {
+      return test !== Object(test)
+    }
+
+    function transformObj(obj: Record<string, unknown>) {
+      for (const key of Object.keys(obj)) {
+        const val = obj[key]
+        if (val instanceof Date) {
+          obj[key] = dayjs(val).subtract(9, 'hour').toDate()
+        } else if (!isPrimitive(val)) {
+          transformObj(val as any)
+        }
+      }
+    }
+
+    if (value instanceof Date) {
+      return dayjs(value).subtract(9, 'hour').toDate() as any
+    } else if (isPrimitive(value)) {
+      return value
+    } else {
+      transformObj(value as any)
+    }
+
+    return value
+  }
+}
+
+export function prismaTimeMod<T>(value: T): T {
+  return Time.utcToKst(value)
 }
