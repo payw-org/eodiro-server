78dedfcb4fcbf19fe1617c46451c37b34d44f7c3
GardenLee <khjvvv7@gmail.com>
2020-06-21 01:21:45 +0900

    feat: add FielRepository
    - deprecate TipFileRepository

--- src/database/repository/tip-file-repository.ts
+++ src/database/repository/tip-file-repository.ts
@@ -1,13 +1,14 @@
 import { TipFile } from '@prisma/client'
 import prisma from '@/modules/prisma'
 
-export class TipFileRepository {
-  static async findByTipId(tipId: number): Promise<TipFile[]> {
-    const fileList = await prisma.tipFile.findMany({
-      where: {
-        tipId: tipId,
-      },
-    })
-    return fileList
-  }
-}
+// deprecated
+// export class TipFileRepository {
+//   static async findByTipId(tipId: number): Promise<TipFile[]> {
+//     const fileList = await prisma.tipFile.findMany({
+//       where: {
+//         tipId: tipId,
+//       },
+//     })
+//     return fileList
+//   }
+// }
