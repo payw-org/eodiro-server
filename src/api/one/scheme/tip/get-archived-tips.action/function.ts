a0116a4587e4987909a36841597f62d8f0f74226
GardenLee <khjvvv7@gmail.com>
2020-06-20 20:40:12 +0900

    feat: add get-archived-tips api

---
+++ src/api/one/scheme/tip/get-archived-tips.action/function.ts
@@ -0,0 +1,38 @@
+import { Action } from './interface'
+import { OneApiFunc } from '@/api/one/types'
+import { TipListResponse } from '@/database/models/tip'
+import { TipRepository } from '@/database/repository/tip-repository'
+import { oneApiResponse } from '@/api/one/utils'
+import prisma from '@/modules/prisma'
+import { prismaTimeMod } from '@/modules/time'
+
+const func: OneApiFunc<Action> = async (data) => {
+  const { page } = data
+  const pageSize = 10
+
+  const totalCount = await prisma.tip.count({
+    where: { isArchived: true, isRemoved: false },
+  })
+
+  const tipList = await TipRepository.findArchived(pageSize, page)
+
+  const tips = prismaTimeMod(
+    tipList.map((item) => {
+      const response: TipListResponse = {
+        ...item,
+        tipLikes: item.tipLikes.length,
+        tipBookmarks: item.tipBookmarks.length,
+        isLiked: true,
+        isBookmarked: true,
+      }
+      return response
+    })
+  )
+
+  const totalPage =
+    totalCount % 10 === 0 ? totalCount / 10 : Math.floor(totalCount / 10) + 1
+
+  return oneApiResponse<Action>({ tips, totalCount, totalPage, page })
+}
+
+export default func
