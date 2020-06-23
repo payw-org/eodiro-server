dfa75c5bf3fdfdb1d5d3a74ebc6508601fd493a1
jhaemin <io@jhaemin.com>
2020-06-23 20:48:50 +0900

    feat: dormitory notice url builder

--- src/modules/cau-notice-watcher/subscribers/dormitory-blue-mir.ts
+++ src/modules/cau-notice-watcher/subscribers/dormitory-blue-mir.ts
@@ -1,11 +1,10 @@
 import { Subscriber } from '..'
 
-// TODO: urlBuilder
-
 export const dormitoryBlueMir: Subscriber = {
   name: '생활관(블루미르홀)',
   key: 'dormitory-blue-mir',
   url: 'http://dormitory.cau.ac.kr/bbs/bbs_list.php?bbsID=notice',
   noticeItemSelector: '#content table tr[bgcolor="#fffcdb"]',
   titleBuilder: (noticeElm) => noticeElm.querySelector('span').textContent,
+  urlBuilder: (noticeElm) => noticeElm.querySelector('a').href,
 }
