581ff410f526f891e754e76de6a5ccedd0428273
GardenLee <khjvvv7@gmail.com>
2020-06-24 00:40:04 +0900

    feat: implement TipFileRepository create

--- src/database/repository/tip-file-repository.ts
+++ src/database/repository/tip-file-repository.ts
@@ -1,14 +1,22 @@
-import { TipFile } from '@prisma/client'
+import { connect } from 'http2'
 import prisma from '@/modules/prisma'
 
-// deprecated
-// export class TipFileRepository {
-//   static async findByTipId(tipId: number): Promise<TipFile[]> {
-//     const fileList = await prisma.tipFile.findMany({
-//       where: {
-//         tipId: tipId,
-//       },
-//     })
-//     return fileList
-//   }
-// }
+export class TipFileRepository {
+  static async create(tipId: number, fileId: number): Promise<boolean> {
+    await prisma.tipFile.create({
+      data: {
+        tip: {
+          connect: {
+            id: tipId,
+          },
+        },
+        file: {
+          connect: {
+            id: fileId,
+          },
+        },
+      },
+    })
+    return true
+  }
+}
