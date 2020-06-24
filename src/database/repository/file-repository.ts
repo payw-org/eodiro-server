fb876158c371a6791d8d1dfdb70a1d8fb0b790f6
jhaemin <io@jhaemin.com>
2020-06-24 20:47:29 +0900

    fix: wrong prisma query for selecting files

--- src/database/repository/file-repository.ts
+++ src/database/repository/file-repository.ts
@@ -4,10 +4,15 @@ import prisma from '@/modules/prisma'
 export class FileRepository {
   static async findTipFiles(tipId: number): Promise<File[]> {
     const fileList = await prisma.file.findMany({
-      include: {
-        tipFiles: { where: { tipId } },
+      where: {
+        tipFiles: {
+          some: {
+            tipId,
+          },
+        },
       },
     })
+
     return fileList
   }
 }
