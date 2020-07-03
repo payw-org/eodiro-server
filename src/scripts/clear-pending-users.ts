f80d2e5d384cd660e17f650bf50595de0fefebc6
jhaemin <io@jhaemin.com>
2020-07-03 09:19:54 +0900

    chore: wrap with try catch

--- src/scripts/clear-pending-users.ts
+++ src/scripts/clear-pending-users.ts
@@ -7,28 +7,32 @@ import prisma from '@/modules/prisma'
 const run = async () => {
   const quit = await boot({ db: true })
 
-  const query = `
-    select *
-    from pending_user
-  `
-  const results = await eodiroQuery<UserAttrs>(query)
+  try {
+    const query = `
+      select *
+      from pending_user
+    `
+    const results = await eodiroQuery<UserAttrs>(query)
 
-  results.forEach(async (row) => {
-    const registeredAt = dayjs(row.registered_at)
-    const now = dayjs()
-    const timeDiffMs = now.diff(registeredAt)
-    const timeDiffMin = timeDiffMs / 1000 / 60
+    results.forEach(async (row) => {
+      const registeredAt = dayjs(row.registered_at)
+      const now = dayjs()
+      const timeDiffMs = now.diff(registeredAt)
+      const timeDiffMin = timeDiffMs / 1000 / 60
 
-    // If over 30 minutes after sending a verfication email
-    // remove from the pending_user table
-    if (timeDiffMin > 30) {
-      await prisma.pendingUser.delete({
-        where: {
-          id: row.id,
-        },
-      })
-    }
-  })
+      // If over 30 minutes after sending a verfication email
+      // remove from the pending_user table
+      if (timeDiffMin > 30) {
+        await prisma.pendingUser.delete({
+          where: {
+            id: row.id,
+          },
+        })
+      }
+    })
+  } catch (err) {
+    console.log(err)
+  }
 
   quit()
 }
