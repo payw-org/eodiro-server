a953b454ec50b983bbb13e6556fcf0113af0d765
Jang Haemin <io@jhaemin.com>
2021-02-09 20:49:12 +0900

    chore: remove boot options

--- src/test/mysql-update-test.ts
+++ src/test/mysql-update-test.ts
@@ -1,12 +1,9 @@
-import Db, { MysqlInsertOrUpdateResult } from '@/db'
-
-import SqlB from '@/modules/sqlb'
 import { boot } from '@/boot'
+import Db, { MysqlInsertOrUpdateResult } from '@/db'
+import SqlB from '@/modules/sqlb'
 
 async function run(): Promise<void> {
-  await boot({
-    mail: false,
-  })
+  await boot()
   const [, results] = await Db.query<MysqlInsertOrUpdateResult>(
     SqlB()
       .update('test', {
