3b8dbc0de1c7ce03b37576435c714b7f445d3061
Jang Haemin <io@jhaemin.com>
2021-02-10 21:24:23 +0900

    migrate join and log in apis from eodiro

---
+++ src/api/auth/log-in.ts
@@ -0,0 +1,85 @@
+import { eodiroConsts } from '@/constants'
+import { SignInInfo } from '@/modules/auth'
+import { setCookie } from '@/modules/cookie'
+import EodiroEncrypt from '@/modules/eodiro-encrypt'
+import { AuthData, signAccessToken, signRefreshToken } from '@/modules/jwt'
+import { prisma } from '@/modules/prisma'
+import { sanitizePoralId } from '@/modules/sanitize-portal-id'
+import express from 'express'
+
+export type ApiAuthLoginReqBody = SignInInfo
+export type ApiAuthLoginResData = {
+  isSigned: boolean
+  refreshToken?: string
+  accessToken?: string
+}
+
+const router = express.Router()
+
+// Sign in
+router.post('/auth/log-in', async (req, res) => {
+  const { portalId, password } = req.body as SignInInfo
+  const resData: ApiAuthLoginResData = { isSigned: false }
+
+  if (!portalId || !password) {
+    res.json(resData)
+    return
+  }
+
+  const sanitizedPortalId = sanitizePoralId(portalId)
+  const user = await prisma.user.findUnique({
+    where: { portalId: sanitizedPortalId },
+  })
+
+  if (!user) {
+    res.json(resData)
+    return
+  }
+
+  const isPasswordMatched = await EodiroEncrypt.isSame(password, user.password)
+
+  if (isPasswordMatched) {
+    const authData: AuthData = { userId: user.id }
+    let refreshToken = ''
+
+    if (user.refreshToken) {
+      refreshToken = user.refreshToken
+    } else {
+      refreshToken = signRefreshToken(authData)
+
+      await prisma.user.update({
+        where: { id: user.id },
+        data: { refreshToken },
+      })
+    }
+
+    const accessToken = signAccessToken(authData)
+
+    resData.refreshToken = refreshToken
+    resData.accessToken = accessToken
+
+    const expires = new Date('2038-01-01').toUTCString()
+    const refreshTokenPaths = ['/api/auth/refresh', '/api/auth/revoke']
+
+    setCookie(req, res, [
+      {
+        name: eodiroConsts.EDR_ACCESS_TOKEN_NAME,
+        value: accessToken,
+        expires,
+        path: '/',
+      },
+      ...refreshTokenPaths.map((path) => ({
+        name: eodiroConsts.EDR_REFRESH_TOKEN_NAME,
+        value: refreshToken,
+        expires,
+        path,
+      })),
+    ])
+  }
+
+  resData.isSigned = isPasswordMatched
+
+  res.json(resData)
+})
+
+export default router
