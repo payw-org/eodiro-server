88576c98ad155e350e411ad9597a001b479aaa0b
Jang Haemin <io@jhaemin.com>
2021-02-10 23:44:03 +0900

    Wrap the constants

--- src/constants/http-status.ts
+++ src/constants/http-status.ts
@@ -1,14 +1,14 @@
 // Reference
 // https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
 
-// Successfull
-export const OK = 200
-
-// Client errors
-export const BAD_REQUEST = 400
-export const UNAUTHORIZED = 401
-export const FORBIDDEN = 403
-export const NOT_FOUND = 404
-
-// Server errors
-export const INTERNAL_SERVER_ERROR = 500
+export const httpStatus = {
+  // Successfull
+  OK: 200,
+  // Client errors
+  BAD_REQUEST: 400,
+  UNAUTHORIZED: 401,
+  FORBIDDEN: 403,
+  NOT_FOUND: 404,
+  // Server errors
+  INTERNAL_SERVER_ERROR: 500,
+}
