cf2c36e223870cae524dfa79f28948a08e43ceda
Jang Haemin <io@jhaemin.com>
2021-02-11 11:57:17 +0900

    Remove One API

--- src/api/one/scripts/copy-packagejson.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs')
-const appRoot = require('app-root-path')
-const path = require('path')
-
-fs.copyFileSync(
-  path.resolve(__dirname, '../package.json'),
-  appRoot.resolve('/build/src/package.json')
-)
