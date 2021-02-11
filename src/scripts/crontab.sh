1ffe1e81c25e287d20c6d05931f96e56a23e14c6
Jang Haemin <io@jhaemin.com>
2021-02-11 21:47:21 +0900

    Change the target paths

--- src/scripts/crontab.sh
+++ src/scripts/crontab.sh
@@ -6,16 +6,16 @@
 # > sudo service cron start
 
 # update random nicknames at 00:00
-0 0 * * * NODE_ENV=production /home/ubuntu/.nvm/versions/node/v13.14.0/bin/node /home/ubuntu/eodiro/server/build/src/scripts/update-random-nicknames
+0 0 * * * NODE_ENV=production /home/ubuntu/.nvm/versions/node/v13.14.0/bin/node /home/ubuntu/eodiro/server/build/scripts/cron/update-random-nicknames
 
 # check notice every 15 minutes
-*/15 * * * *NODE_ENV=production /home/ubuntu/.nvm/versions/node/v13.14.0/bin/node /home/ubuntu/eodiro/server/build/src/scripts/check-notice
+*/15 * * * *NODE_ENV=production /home/ubuntu/.nvm/versions/node/v13.14.0/bin/node /home/ubuntu/eodiro/server/build/scripts/cron/check-notice
 
 # seed cafeteria menus at 03:00 everyday
-0 3 * * * NODE_ENV=production /home/ubuntu/.nvm/versions/node/v13.14.0/bin/node /home/ubuntu/eodiro/server/build/src/scripts/seed-cafeteria-menus
+0 3 * * * NODE_ENV=production /home/ubuntu/.nvm/versions/node/v13.14.0/bin/node /home/ubuntu/eodiro/server/build/scripts/cron/seed-cafeteria-menus
 
 # garbage collect files every 3 hours
-0 */3 * * * NODE_ENV=production /home/ubuntu/.nvm/versions/node/v13.14.0/bin/node /home/ubuntu/eodiro/server/build/src/scripts/garbage-collect-files
+0 */3 * * * NODE_ENV=production /home/ubuntu/.nvm/versions/node/v13.14.0/bin/node /home/ubuntu/eodiro/server/build/scripts/cron/garbage-collect-files
 
 # clear pending users every 30 minutes
-*/30 * * * * NODE_ENV=production /home/ubuntu/.nvm/versions/node/v13.14.0/bin/node /home/ubuntu/eodiro/server/build/src/scripts/clear-pending-users
+*/30 * * * * NODE_ENV=production /home/ubuntu/.nvm/versions/node/v13.14.0/bin/node /home/ubuntu/eodiro/server/build/scripts/cron/clear-pending-users
