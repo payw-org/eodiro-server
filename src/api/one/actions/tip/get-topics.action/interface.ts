69162f4d87eb039083b9c66896aac612ea71d360
Jang Haemin <io@jhaemin.com>
2021-02-11 11:57:17 +0900

    Remove One API

--- src/api/one/actions/tip/get-topics.action/interface.ts
+++
@@ -1,8 +0,0 @@
-import {
-  OneApiActionTemplatePayload,
-  OneApiActionTemplateWithoutReqeustData,
-} from '@/api/one/types'
-
-export type Action = OneApiActionTemplateWithoutReqeustData<
-  OneApiActionTemplatePayload<Record<string, string>>
->
