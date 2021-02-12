2244f5fe40895c9e6e4f877c7ed600a153f6e234
Jang Haemin <io@jhaemin.com>
2021-02-12 19:25:38 +0900

    Replace deprecated body-parser with built-in middleware

--- src/boot.ts
+++ src/boot.ts
@@ -1,4 +1,3 @@
-import bodyParser from 'body-parser'
 import chalk from 'chalk'
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
@@ -28,8 +27,8 @@ export async function boot(): Promise<QuitFunction> {
   )
   app.use(morgan('tiny'))
   app.use(cookieParser())
-  app.use(bodyParser.json({ limit: '100mb' }))
-  app.use(bodyParser.urlencoded({ limit: '100mb', extended: true }))
+  app.use(express.json({ limit: '100mb' }))
+  app.use(express.urlencoded({ limit: '100mb', extended: true }))
   app.use(api)
 
   let server: http.Server = undefined
