4873d879a780f3af9fac54ac8357d5eb84ac72af
Jang Haemin <io@jhaemin.com>
2021-02-13 22:48:52 +0900

    Off explicit-module-boundary-types

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,6 +25,7 @@ module.exports = {
         asyncArrow: 'always',
       },
     ],
+    '@typescript-eslint/explicit-module-boundary-types': [0],
     '@typescript-eslint/explicit-function-return-type': [0],
     '@typescript-eslint/no-var-requires': [0],
     '@typescript-eslint/no-namespace': [0],
