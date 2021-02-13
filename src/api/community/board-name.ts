4c46a303491af7453e4f6d6c35faa2d33fd2da5b
Jang Haemin <io@jhaemin.com>
2021-02-13 23:35:01 +0900

    Create api for fetching board name with board id

---
+++ src/api/community/board-name.ts
@@ -0,0 +1,43 @@
+import { httpStatus } from '@/constant/http-status'
+import { handleExpressValidation } from '@/middleware/handle-express-validation'
+import { makeQueryValidator } from '@/modules/express-validator-utils'
+import { prisma } from '@/modules/prisma'
+import express from 'express'
+
+const router = express.Router()
+
+export type ApiCommunityBoardNameReqQuery = {
+  boardId: number
+}
+
+export type ApiCommunityBoardNameResData = {
+  boardName: string
+}
+
+const query = makeQueryValidator<ApiCommunityBoardNameReqQuery>()
+
+router.get<
+  any,
+  ApiCommunityBoardNameResData,
+  any,
+  ApiCommunityBoardNameReqQuery
+>(
+  '/community/board-name',
+  query('boardId').isNumeric().toInt(),
+  handleExpressValidation,
+  async (req, res) => {
+    const board = await prisma.communityBoard.findUnique({
+      where: { id: req.query.boardId },
+    })
+
+    if (!board) {
+      return res.sendStatus(httpStatus.NOT_FOUND)
+    }
+
+    res.json({
+      boardName: board.name,
+    })
+  }
+)
+
+export default router
