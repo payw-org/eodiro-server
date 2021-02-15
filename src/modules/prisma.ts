646d36e072f7136cd1eb255b74a022a156274109
Jang Haemin <io@jhaemin.com>
2021-02-15 11:50:00 +0900

    Apply timemod

--- src/modules/prisma.ts
+++ src/modules/prisma.ts
@@ -1,3 +1,16 @@
 import { PrismaClient } from '@prisma/client'
+import { prismaTimeMod } from './time'
 
-export const prisma = new PrismaClient()
+function timeMod(prismaInstance: PrismaClient) {
+  prismaInstance.$use(async (params, next) => {
+    const result = await next(params)
+
+    // Subtract 9 hours from UTC
+    return prismaTimeMod(result)
+  })
+}
+
+const prisma = new PrismaClient()
+timeMod(prisma)
+
+export { prisma }
