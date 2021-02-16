967a166fc44318d28d0c72ce6d259a7f1f7f0271
Jang Haemin <io@jhaemin.com>
2021-02-16 20:59:09 +0900

    Add more custom Prisma schema types

--- src/types/schema.ts
+++ src/types/schema.ts
@@ -1,13 +1,38 @@
 import {
+  CommunityBoard,
   CommunityComment,
   CommunityPost,
+  CommunityPostBookmark,
+  CommunityPostLike,
   CommunitySubcomment,
-} from '@prisma/client'
+} from '@/prisma/client'
 
 type Safe<T> = Omit<T, 'userId' | 'isDeleted'> & {
   isMine: boolean
 }
 
+// Safe
+export type SafeCommunityBoard = Omit<
+  CommunityBoard,
+  | 'isDeleted'
+  | 'priority'
+  | 'activeAt'
+  | 'createdBy'
+  | 'description'
+  | 'createdAt'
+> & {
+  description?: string | null
+  createdAt?: Date
+}
 export type SafeCommunityPost = Safe<CommunityPost>
 export type SafeCommunityComment = Safe<CommunityComment>
 export type SafeCommunitySubcomment = Safe<CommunitySubcomment>
+
+// Packed
+export type CommunityPostsList = (SafeCommunityPost & {
+  communityComments: (SafeCommunityComment & {
+    communitySubcomments: SafeCommunitySubcomment[]
+  })[]
+  communityPostLikes: CommunityPostLike[]
+  communityPostBookmarks: CommunityPostBookmark[]
+})[]
