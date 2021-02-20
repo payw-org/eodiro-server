93d42f664ff71de85411ffe45f1299e274844c4e
Jang Haemin <io@jhaemin.com>
2021-02-20 21:22:29 +0900

    replace legacy db with prisma

--- src/api/cafeteria/index.ts
+++ src/api/cafeteria/index.ts
@@ -1,6 +1,5 @@
-import { TableNames } from '@/database/table-names'
-import Db from '@/db'
-import SqlB from '@/modules/sqlb'
+import { httpStatus } from '@/constants/http-status'
+import { prisma } from '@/modules/prisma'
 import { Campus } from '@/types'
 import dayjs from 'dayjs'
 import express from 'express'
@@ -11,26 +10,23 @@ router.get('/cafeteria/:servedAt/:campus/menus', async (req, res) => {
   const now = dayjs()
   const campus: Campus = (req.params?.campus as Campus) || '서울'
   const servedAt = req.params?.servedAt || now.format('YYYY-MM-DD')
-  const sql = SqlB()
-    .select('data')
-    .from(TableNames.cafeteria_menu)
-    .where()
-    .equal('campus', campus)
-    .andEqual('served_at', servedAt)
-    .build()
 
-  const [err, results] = await Db.query(sql)
-  if (err) {
-    res.sendStatus(500)
+  const result = await prisma.cafeteriaMenu.findFirst({
+    where: {
+      campus,
+      servedAt,
+    },
+    select: {
+      data: true,
+    },
+  })
+
+  if (!result) {
+    res.sendStatus(httpStatus.NOT_FOUND)
     return
   }
 
-  if (results.length === 0) {
-    res.sendStatus(204)
-    return
-  }
-
-  res.json(JSON.parse(results[0]?.data))
+  res.json(result.data)
 })
 
 export default router
