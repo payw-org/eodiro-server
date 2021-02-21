f6dbde010af90df3d519aac0028d4abc1f0ca35c
Jang Haemin <io@jhaemin.com>
2021-02-21 23:51:23 +0900

    verify join api

---
+++ src/api/auth/verify-join.ts
@@ -0,0 +1,48 @@
+import { httpStatus } from '@/constant/http-status'
+import { handleExpressValidation } from '@/middleware/handle-express-validation'
+import { prisma } from '@/modules/prisma'
+import express from 'express'
+import { body } from 'express-validator'
+
+export type ApiAuthVerifyJoinReqBody = {
+  token: string
+}
+
+const router = express.Router()
+
+router.post<any, any, ApiAuthVerifyJoinReqBody>(
+  '/auth/verify-join',
+  body('token').isString(),
+  handleExpressValidation,
+  async (req, res) => {
+    const { token } = req.body
+
+    const pendingUser = await prisma.pendingUser.findUnique({
+      where: { token },
+    })
+
+    if (!pendingUser) {
+      return res.sendStatus(httpStatus.NOT_FOUND)
+    }
+
+    const clearPendingUser = prisma.pendingUser.delete({
+      where: { token },
+    })
+    const {
+      portalId,
+      nickname,
+      password,
+      randomNickname,
+      joinedAt,
+    } = pendingUser
+    const createUser = prisma.user.create({
+      data: { portalId, nickname, password, randomNickname, joinedAt },
+    })
+
+    await prisma.$transaction([clearPendingUser, createUser])
+
+    res.sendStatus(httpStatus.OK)
+  }
+)
+
+export default router
