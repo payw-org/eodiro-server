9753480b3cf4ea3a996bce6d71e72be021c9139b
Jang Haemin <io@jhaemin.com>
2021-02-27 22:11:05 +0900

    use auth routers

--- src/api/auth/index.ts
+++ src/api/auth/index.ts
@@ -1,20 +1,28 @@
+import { requireAuth } from '@/middleware/require-auth'
 import express from 'express'
 import changePassword from './change-password'
 import forgot from './forgot'
 import join from './join'
-import signIn from './log-in'
+import logIn from './log-in'
+import logOut from './log-out'
 import refresh from './refresh'
 import validate from './validate'
 import verify from './verify'
+import verifyJoin from './verify-join'
 
 const router = express.Router()
 
-router.use(changePassword)
 router.use(forgot)
 router.use(join)
-router.use(signIn)
-router.use(verify)
+router.use(logIn)
+router.use(logOut)
 router.use(refresh)
 router.use(validate)
+router.use(verify)
+router.use(verifyJoin)
+
+router.use(requireAuth)
+
+router.use(changePassword)
 
 export default router
