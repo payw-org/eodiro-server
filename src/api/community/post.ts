abf20481622aa3716ab4d6b567bea46655c78087
Jang Haemin <io@jhaemin.com>
2021-03-06 22:51:50 +0900

    Add upsert/delete post api

--- src/api/community/post.ts
+++ src/api/community/post.ts
@@ -1,12 +1,20 @@
+import { eodiroConst } from '@/constant'
 import { httpStatus } from '@/constant/http-status'
 import { handleExpressValidation } from '@/middleware/handle-express-validation'
-import { makeQueryValidator } from '@/modules/express-validator-utils'
+import {
+  makeBodyValidator,
+  makeQueryValidator,
+} from '@/modules/express-validator-utils'
 import { prisma } from '@/modules/prisma'
 import { secureTable } from '@/modules/secure-table'
+import { dbNow } from '@/modules/time'
 import { SafeCommunityPost } from '@/types/schema'
 import express from 'express'
 
 const router = express.Router()
+const postRouterPath = '/community/post'
+
+/* ------ Get a post ------ */
 
 export type ApiCommunityGetPostReqQuery = {
   postId: number
@@ -24,7 +32,7 @@ export type ApiCommunityGetPostResData = SafeCommunityPost & {
 const query = makeQueryValidator<ApiCommunityGetPostReqQuery>()
 
 router.get<any, ApiCommunityGetPostResData, any, ApiCommunityGetPostReqQuery>(
-  '/community/post',
+  postRouterPath,
   query('postId').isNumeric().toInt(),
   handleExpressValidation,
   async (req, res) => {
@@ -76,5 +84,118 @@ router.get<any, ApiCommunityGetPostResData, any, ApiCommunityGetPostReqQuery>(
     })
   }
 )
+
+/* ------ Get a post ------ */
+
+/* ------ Upsert a post ------ */
+
+export type ApiCommunityUpsertPostReqBody = {
+  postId?: number
+  boardId: number
+  title: string
+  body: string
+}
+
+export type ApiCommunityUpsertPostResData = {
+  postId: number
+}
+
+const upsertPostBody = makeBodyValidator<ApiCommunityUpsertPostReqBody>()
+
+router.post(
+  postRouterPath,
+  upsertPostBody('postId').optional().isNumeric(),
+  upsertPostBody('boardId').isNumeric(),
+  upsertPostBody('title').isString().trim().isLength({
+    min: eodiroConst.MIN_POST_TITLE_LENGTH,
+    max: eodiroConst.MAX_POST_TITLE_LENGTH,
+  }),
+  upsertPostBody('body').isString().trim().isLength({
+    min: eodiroConst.MIN_POST_BODY_LENGTH,
+    max: eodiroConst.MAX_COMMENT_BODY_LENGTH,
+  }),
+  handleExpressValidation,
+  async (req, res) => {
+    const { postId, boardId, title, body } = req.body
+    const { user } = req
+
+    // Upsert post
+    const upsertResult = await prisma.communityPost.upsert({
+      where: { id: postId ?? 0 },
+      create: {
+        title,
+        body,
+        randomNickname: user.randomNickname,
+        postedAt: dbNow(),
+        user: {
+          connect: { id: user.id },
+        },
+        communityBoard: {
+          connect: { id: boardId },
+        },
+      },
+      update: {
+        title,
+        body,
+        editedAt: dbNow(),
+      },
+    })
+
+    // Update board active at
+    await prisma.communityBoard.update({
+      where: { id: upsertResult.boardId },
+      data: { activeAt: dbNow() },
+    })
+
+    res.json({
+      postId: upsertResult.id,
+    })
+  }
+)
+
+/* ------ Upsert a post ------ */
+
+/* ------ Delete a post ------ */
+
+export type ApiCommunityDeletePostReqBody = {
+  postId: number
+}
+
+const deletePostBody = makeBodyValidator<ApiCommunityDeletePostReqBody>()
+
+router.delete(
+  postRouterPath,
+  deletePostBody('postId').isNumeric(),
+  handleExpressValidation,
+  async (req, res) => {
+    const { postId } = req.body
+    const post = await prisma.communityPost.findUnique({
+      where: { id: postId },
+    })
+    const { user } = req
+
+    // No post
+    if (!post) {
+      return res.sendStatus(httpStatus.NOT_FOUND)
+    }
+
+    // Forbidden
+    if (post.userId !== user.id) {
+      return res.sendStatus(httpStatus.FORBIDDEN)
+    }
+
+    // Delete
+    await prisma.communityPost.update({
+      where: { id: postId },
+      data: {
+        isDeleted: true,
+      },
+    })
+
+    res.sendStatus(httpStatus.OK)
+  }
+)
+
+/* ------ Delete a post ------ */
 
 export default router
