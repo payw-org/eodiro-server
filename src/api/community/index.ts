cbff0a9c46a2d5bb6ac3812641b6c79ff3c5a13d
Jang Haemin <io@jhaemin.com>
2021-03-12 22:54:18 +0900

    Apply board and popular posts apis

--- src/api/community/index.ts
+++ src/api/community/index.ts
@@ -1,11 +1,13 @@
 import { requireAuth } from '@/middleware/require-auth'
 import express from 'express'
 import allBoards from './all-boards'
+import board from './board'
 import boardName from './board-name'
 import bookmarkPost from './bookmark-post'
 import comment from './comment'
 import likePost from './like-post'
 import pinnedBoards from './pinned-boards'
+import popularPosts from './popular-posts'
 import post from './post'
 import postsList from './posts-list'
 import recentBoards from './recent-boards'
@@ -17,10 +19,12 @@ router.use(requireAuth)
 
 router.use(allBoards)
 router.use(boardName)
+router.use(board)
 router.use(bookmarkPost)
 router.use(comment)
 router.use(likePost)
 router.use(pinnedBoards)
+router.use(popularPosts)
 router.use(post)
 router.use(postsList)
 router.use(recentBoards)
