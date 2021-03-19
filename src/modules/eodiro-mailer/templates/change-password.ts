c9f9d3ccd1b88b7c2f3883b86dc144dec099a867
Jang Haemin <io@jhaemin.com>
2021-03-19 23:26:55 +0900

    Use html syntax

--- src/modules/eodiro-mailer/templates/change-password.ts
+++ src/modules/eodiro-mailer/templates/change-password.ts
@@ -1,7 +1,9 @@
-export default `
-<div
-  class="join-body"
-  style="
+import { html } from '../utils'
+
+export default html`
+  <div
+    class="join-body"
+    style="
     padding: 0;
     margin: 0;
     background-color: #fff;
@@ -10,10 +12,10 @@ export default `
     padding-top: 50px;
     width: 100%;
   "
->
-  <div
-    class="join-container"
-    style="
+  >
+    <div
+      class="join-container"
+      style="
       box-sizing: border-box;
       width: calc(100% - 40px);
       max-width: 500px;
@@ -23,39 +25,42 @@ export default `
       padding: 20px;
       text-align: center;
     "
-  >
-    <img
-      src="https://eodiro.com/icon-512.png"
-      class="logo"
-      style="
+    >
+      <img
+        src="https://eodiro.com/icon-512.png"
+        class="logo"
+        style="
         width: 100%;
         max-width: 100px;
         margin: auto;
       "
-    />
-    <div
-      class="content"
-      style="
+      />
+      <div
+        class="content"
+        style="
         margin-top: 20px;
         padding-bottom: 20px;
       "
-    >
-      <p
-        class="description"
-        style="
+      >
+        <p
+          class="description"
+          style="
           font-size: 15px;
           font-weight: 400;
           color: #808080;
           margin: 0;
           margin-top: 10px;
         "
-      >
-        아래 버튼을 클릭해 암호 변경을 마치세요.
-      </p>
-      <a href="https://eodiro.com/forgot/change-password?t={{token}}" target="_blank">
-        <button
-          class="join-btn"
-          style="
+        >
+          아래 버튼을 클릭해 암호 변경을 마치세요.
+        </p>
+        <a
+          href="https://eodiro.com/forgot/change-password?t={{token}}"
+          target="_blank"
+        >
+          <button
+            class="join-btn"
+            style="
             padding: 10px 15px;
             appearance: none;
             -webkit-appearance: none;
@@ -70,31 +75,31 @@ export default `
             border-radius: 8px;
             cursor: pointer;
           "
-        >
-          암호 변경
-        </button>
-      </a>
+          >
+            암호 변경
+          </button>
+        </a>
+      </div>
     </div>
-  </div>
-  <div
-    class="footer"
-    style="
+    <div
+      class="footer"
+      style="
       font-size: 14px;
       color: #808080;
       text-align: center;
       padding: 50px 20px;
     "
-  >
-    <a
-      href="https://payw.org"
-      target="_blank"
-      style="
+    >
+      <a
+        href="https://payw.org"
+        target="_blank"
+        style="
         color: inherit;
         text-decoration: none;
       "
-    >
-      © PAYW
-    </a>
+      >
+        © PAYW
+      </a>
+    </div>
   </div>
-</div>
 `
