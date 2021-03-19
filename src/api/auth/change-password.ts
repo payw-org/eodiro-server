0e0a8fa3de300e500240055f963fb29b451299f8
Jang Haemin <io@jhaemin.com>
2021-03-19 23:30:01 +0900

    Update change password api

--- src/api/auth/change-password.ts
+++ src/api/auth/change-password.ts
@@ -1,28 +1,50 @@
-import { requireAuth } from '@/middleware/require-auth'
+import { httpStatus } from '@/constant/http-status'
+import { handleExpressValidation } from '@/middleware/handle-express-validation'
 import Auth from '@/modules/auth'
+import { makeBodyValidator } from '@/modules/express-validator-utils'
 import { prisma } from '@/modules/prisma'
 import express from 'express'
 
 const router = express.Router()
 
-export type ApiAuthChangePasswordReqData = {
+export type ApiAuthChangePasswordReqBody = {
+  token: string
   newPassword: string
 }
 
-router.post<any, any, ApiAuthChangePasswordReqData>(
-  '/change-password',
-  requireAuth,
-  async (req, res) => {
-    const { newPassword } = req.body
-    const userId = req.user.id
+const changePasswordBody = makeBodyValidator<ApiAuthChangePasswordReqBody>()
 
-    await prisma.changePassword.delete({ where: { userId } })
-    await prisma.user.update({
+router.post<any, any, ApiAuthChangePasswordReqBody>(
+  '/change-password',
+  changePasswordBody('token').isString(),
+  changePasswordBody('newPassword').isString(),
+  handleExpressValidation,
+  async (req, res) => {
+    const { token, newPassword } = req.body
+
+    const changePasswordInfo = await prisma.changePassword.findUnique({
+      where: {
+        token,
+      },
+    })
+
+    if (!changePasswordInfo) {
+      return res.sendStatus(httpStatus.NOT_FOUND)
+    }
+
+    const userId = changePasswordInfo.userId
+
+    const deleteChangePasswordInfo = prisma.changePassword.delete({
+      where: { userId },
+    })
+    const updatePassword = prisma.user.update({
       where: { id: userId },
       data: {
         password: await Auth.encryptPw(newPassword),
       },
     })
+
+    await prisma.$transaction([deleteChangePasswordInfo, updatePassword])
 
     res.sendStatus(200)
   }
