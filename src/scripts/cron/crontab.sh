26b78a219543833dd8cad75af29164031ee3ac7b
Jang Haemin <io@jhaemin.com>
2021-04-13 07:54:54 +0900

    Add skeleton crontab shell script

---
+++ src/scripts/cron/crontab.sh
@@ -0,0 +1,24 @@
+# run `crontab -e` to edit crontab
+# copy and paste the crons below
+#
+# (optional) restart cron service when its timezone does not follow the system timezone
+# > sudo service cron stop
+# > sudo service cron start
+
+# [path-to-node]: ex) /home/~/node/v13/bin/node
+# [path-to-cron]: ex) /home/~/cron
+
+# update random nicknames at 00:00
+0 0 * * * [path-to-node] [path-to-cron]/update-random-nicknames
+
+# check board candidate votes at 00:00
+0 0 * * * [path-to-node] [path-to-cron]/check-board-candidate-votes
+
+# check notice every 15 minutes
+*/15 * * * * [path-to-node] [path-to-cron]/check-notice
+
+# clear pending users every 30 minutes
+*/30 * * * * [path-to-node] [path-to-cron]/clear-pending-users
+
+# seed cafeteria menus at 03:00 everyday
+# 0 3 * * * NODE_ENV=production [path-to-node] [path-to-cron]/seed-cafeteria-menus
