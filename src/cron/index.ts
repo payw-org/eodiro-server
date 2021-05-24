660c67dda3ce48b664c12ce8a9f62698b42acce8
Jang Haemin <io@jhaemin.com>
2021-05-24 10:05:41 +0900

    Replace node-schedule with node-cron

--- src/cron/index.ts
+++ src/cron/index.ts
@@ -1,4 +1,4 @@
-import schedule from 'node-schedule'
+import { CronJob } from 'cron'
 import { backupDb } from './backup-db'
 import { checkBoardCandidateVotes } from './check-board-candidate-votes'
 import { checkNotice } from './check-notice'
@@ -6,20 +6,20 @@ import { clearPendingUsers } from './clear-pending-users'
 import { updateRandomNicknames } from './update-random-nicknames'
 
 // Every 15 minutes
-schedule.scheduleJob('*/15 * * * *', checkNotice)
+new CronJob('*/15 * * * *', checkNotice, null, true, 'Asia/Seoul')
 
 // Every 30 minutes
-schedule.scheduleJob('*/30 * * * *', clearPendingUsers)
+new CronJob('*/30 * * * *', clearPendingUsers, null, true, 'Asia/Seoul')
 
 // every 00:00
-schedule.scheduleJob('00 00 * * *', backupDb)
-schedule.scheduleJob('00 00 * * *', checkBoardCandidateVotes)
-schedule.scheduleJob('00 00 * * *', updateRandomNicknames)
+new CronJob('0 0 * * *', backupDb, null, true, 'Asia/Seoul')
+new CronJob('0 0 * * *', checkBoardCandidateVotes, null, true, 'Asia/Seoul')
+new CronJob('0 0 * * *', updateRandomNicknames, null, true, 'Asia/Seoul')
 
 // Kill processes every 15 minutes in production
 // if (isDev) {
-//   schedule.scheduleJob('*/15 * * * *', () => {
+//   new CronJob('*/15 * * * *', () => {
 //     const queries = ['prisma', 'chromium']
 //     queries.forEach((query) => kill(query))
-//   })
+//   }, null, true, 'Asia/Seoul')
 // }
